00df471ddca6fd453bb8ff405ff85c216780d264
unknown <livistteddy@gmail.com>
2021-05-05 19:29:27 +0100

    Add player_assignment function

--- main.rb
+++ main.rb
@@ -1,3 +1,18 @@
+def player_assignment
+  puts "Welcome to this tic-tac-toe game"
+  puts "Please, put in your name Player 1"
+  player_one = gets.chomp
+  sleep 0.5
+  puts "Player 1 your marker is 'X'"
+  puts ""
+  puts "Please, put in your name Player 2"
+  player_two = gets.chomp
+  sleep 0.5
+  puts "Player 2 your marker is 'O'"
+  puts ""
+  [player_one, player_two]
+end
+
 def board(arr)
   puts "+---+---+---+"
   puts "| #{arr[0]} | #{arr[1]} | #{arr[2]} |"
