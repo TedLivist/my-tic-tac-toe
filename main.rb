b935c4d3a20f185ee995368e53a8bf182126497f
unknown <livistteddy@gmail.com>
2021-05-08 14:58:58 +0100

    Add board class

--- main.rb
+++ main.rb
@@ -15,16 +15,6 @@ def player_assignment
   [player_one, player_two]
 end
 
-def board(arr)
-  puts "+---+---+---+"
-  puts "| #{arr[0]} | #{arr[1]} | #{arr[2]} |"
-  puts "+---+---+---+"
-  puts "| #{arr[3]} | #{arr[4]} | #{arr[5]} |"
-  puts "+---+---+---+"
-  puts "| #{arr[6]} | #{arr[7]} | #{arr[8]} |"
-  puts "+---+---+---+"
-end
-
 def play
   cell = [1,2,3,4,5,6,7,8,9]
   players = player_assignment
